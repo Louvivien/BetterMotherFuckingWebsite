4c5b520430ff0e38d8113e6c36c5530fcbcf9369
Louvivien <43605778+Louvivien@users.noreply.github.com>
2018-09-28 14:46:06 +0200

    message pop up simple

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
+document.onload = setTimeout(function () { alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'); }, 10000);
+
+
+/*
 var monTitre = document.querySelector('h1');
 monTitre.textContent = 'Hello world!';
+*/
+
