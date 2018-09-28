64bdae1cb870dc85e58315b82582caa5de9c8d9c
Louvivien <43605778+Louvivien@users.noreply.github.com>
2018-09-28 15:26:32 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ document.onload= setTimeout("stay_here()",5000);
 // pop up pr aller ou non sur un lien
 
 
+
 function myFunction() {
     document.body.style.backgroundColor = "magenta";
     document.p.style.Color = "blue";
