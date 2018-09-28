22e123751322bf19880067c28870f581195a1dfe
Louvivien <43605778+Louvivien@users.noreply.github.com>
2018-09-28 14:52:19 +0200

    ajout pop up complexe

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
-document.onload = setTimeout(function () { alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'); }, 10000);
+document.onload= setTimeout("stay_here()",5000);
 
+	function stay_here() {
+
+		if (!window.confirm("Tu veux aller sur xxxvidsxx ?")) {
+			window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","Hey, it works.","width=600,height=600");
+		}
+ 		else {
+			alert("OK. tant pis");
+			return false;
+		}
+	}
+
+
+/*
+document.onload = setTimeout(function () { alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'); }, 10000);
+*/ 
+// pop up simple
 
 
 
@@ -7,4 +23,4 @@ document.onload = setTimeout(function () { alert('Hey le site xxxvidsxxx est tro
 var monTitre = document.querySelector('h1');
 monTitre.textContent = 'Hello world!';
 */
-
+// greeting
